72abf1421711a3a790cb3c15fc0afef5935097df
Leon Anavi <leon.anavi@konsulko.com>
2015-10-14 17:39:07 +0300

    Systemd service, timer and a script to detect network issues and to reconnect
    Signed-off-by: Leon Anavi <leon.anavi@konsulko.com>

---
+++ connman-reconnect.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+configServer='8.8.8.8'
+configPingTimeout=3
+
+ipaddr=`ifconfig eth0 | grep 'inet addr:'  | cut -d: -f2 | awk '{ print $1}'`
+#echo $ipaddr
+if [ -z $ipaddr ]; then 
+	echo "IP not assigned"
+	exit 0
+fi
+ping -q -c 1 -W $configPingTimeout $configServer
+isNetworkALive=$?
+if [ $isNetworkALive -eq 0 ]; then
+	echo "Network is alive"
+	exit 0
+fi
+service=`connmanctl services | grep "\*AO" | cut -d: -f2 | awk '{ print $3}'`
+if [ -z service ]; then
+	echo "Unable to retrieve network"
+	exit 0
+fi
+echo $service
+connmanctl disconnect $service
+sleep 3
+connmanctl connect $service
