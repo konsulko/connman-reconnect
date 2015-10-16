56831f743eba59bb355a44774b4a3f9908a3641d
Leon Anavi <leon.anavi@konsulko.com>
2015-10-16 10:48:24 +0300

    Retrieve and ping default gateway, ignore any issues and do not restart if the device in NOT connect to a WiFi network
    Signed-off-by: Leon Anavi <leon.anavi@konsulko.com>

--- connman-reconnect.sh
+++ connman-reconnect.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-configServer='8.8.8.8'
 configPingTimeout=3
 
+#retrieve the default gateway
+server=`route -n | grep 'UG[ \t]' | awk '{print $2}'`
+if [ -z $server ]; then
+	#if the default gateway cannot be found use Google public DNS server
+	server='8.8.8.8'
+fi
 ipaddr=`ifconfig eth0 | grep 'inet addr:'  | cut -d: -f2 | awk '{ print $1}'`
-#echo $ipaddr
 if [ -z $ipaddr ]; then 
 	echo "IP not assigned"
 	exit 0
 fi
-ping -q -c 1 -W $configPingTimeout $configServer
+ping -q -c 1 -W $configPingTimeout $server
 isNetworkALive=$?
 if [ $isNetworkALive -eq 0 ]; then
 	echo "Network is alive"
@@ -20,7 +24,10 @@ if [ -z service ]; then
 	echo "Unable to retrieve connected WiFi"
 	exit 0
 fi
-echo $service
+if [[ $service != "wifi"* ]]; then
+	echo "$service is not a WiFi network"
+	exit 0
+fi
 connmanctl disconnect $service
 sleep 3
 connmanctl connect $service
